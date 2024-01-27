0902002dea5b6878513bddc2ff86609489f70bc4
lawrence500 <31918764+lawrence500@users.noreply.github.com>
2024-01-27 20:59:42 +0000

    Add files via upload

--- rockpaper.js
+++ rockpaper.js
@@ -4,6 +4,10 @@ const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 const playerScoreDisplay = document.getElementById("playerScoreDisplay")
 const ComputerScoreDisplay = document.getElementById("ComputerScoreDisplay")
+const YouWin = document.getElementById('YouWin');
+const ComputerWins = document.getElementById('ComputerWins')
+const gameBox = document.getElementById('gameBox')
+const restart = document.getElementById('restart')
 
 let playerScore = 0;
 let computerScore = 0;
@@ -43,5 +47,50 @@ function playGame(playerChoice){
         computerScore++;
         ComputerScoreDisplay.textContent = computerScore;
   }
+
+  if(playerScore == 10){
+    gameBox.style.display = 'none'
+    YouWin.style.display = 'block'
+    YouWin.style.color = 'white'
+    restart.style.display = 'block'
+  }
+  else if(playerScore >= 10){
+  playerScoreDisplay.textContent = 0;
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreDisplay.textContent = playerScore
+  ComputerScoreDisplay.textContent = computerScore
+  }
+
+  if(computerScore == 10){
+    gameBox.style.display = 'none'
+    ComputerWins.style.display = 'block'
+    ComputerWins.style.color = 'white'
+    restart.style.display = 'block'
+  }
+  else if(computerScore >= 10){
+    ComputerScoreDisplay.textContent = 0;
+  computerScore = 0;
+  playerScore = 0;
+  playerScoreDisplay.textContent = playerScore
+  ComputerScoreDisplay.textContent = computerScore
+  }
+
+  restart.onclick = function(){
+    computerScore = 0;
+    playerScore = 0;
+    restart.style.display = 'none'
+    gameBox.style.display = 'block'
+    YouWin.style.display = 'none'
+    ComputerWins.style.display = 'none'
+    ComputerScoreDisplay.textContent = computerScore
+    playerScoreDisplay.textContent = playerScore
+  }
+
 }
+
+
+
+
+
 
